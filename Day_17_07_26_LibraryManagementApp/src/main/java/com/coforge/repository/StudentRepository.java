b39f4cbec439789fea6805293b8801804243e559
Atharva Rupesh Rajpurkar <Atharva.Rajpurkar@Coforge.com>
2026-07-17 12:29:24 -0700

    Library Management App with CRUD Operations

---
+++ Day_17_07_26_LibraryManagementApp/src/main/java/com/coforge/repository/StudentRepository.java
@@ -0,0 +1,13 @@
+package com.coforge.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.coforge.entities.Student;
+
+
+
+@Repository
+public interface StudentRepository extends JpaRepository<Student, Integer> {
+
+}
